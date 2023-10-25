f27f24000355f835a35191a85e9e90234fc287cd
philcifone <phil@philcifone.com>
2023-10-25 13:15:44 -0400

    add delete button, and made form fields required

--- script.js
+++ script.js
@@ -33,12 +33,29 @@ function listIdeas() {
         output.innerHTML += `<p><strong>${index + 1}. Title:</strong> ${idea.title}</p>`;
         output.innerHTML += `<p><strong>Description:</strong> ${idea.description}</p>`;
         output.innerHTML += `<p><strong>Tags:</strong> ${idea.tags.join(', ')}</p><br>`;
+        output.innerHTML += `<button onclick="deleteIdea(${index})">Delete</button><br><br>`;
     });
 }
 
 // Function to save ideas to local storage
 function saveIdeas() {
     localStorage.setItem('ideas', JSON.stringify(ideas));
+}
+
+// Function to delete an idea by index
+function deleteIdea(index) {
+    // Display a confirmation dialog
+    const confirmation = confirm("Are you sure you want to delete this idea?");
+    
+    if (confirmation) {
+        ideas.splice(index, 1);
+
+        // Save ideas to local storage after deleting
+        saveIdeas();
+
+        // Update the list of ideas
+        listIdeas();
+    }
 }
 
 document.getElementById('ideaForm').addEventListener('submit', addIdea);
