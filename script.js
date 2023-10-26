7637a8b00e008769a4d5a27c4c9aa57ad70d5138
philcifone <phil@philcifone.com>
2023-10-26 03:25:33 -0400

    added search, added hide toggle to list button, and added time stamp instead of numbered list.

--- script.js
+++ script.js
@@ -10,6 +10,7 @@ if (localStorage.getItem('ideas')) {
 function addIdea(event) {
     event.preventDefault();
 
+    const timestamp = new Date().toLocaleString();
     const title = document.getElementById("title").value;
     const description = document.getElementById("description").value;
     const tags = document.getElementById("tags").value.split(',');
@@ -19,7 +20,7 @@ function addIdea(event) {
     const reader = new FileReader();
     reader.onload = function () {
         const imageDataUrl = reader.result; // get data url
-        ideas.push({ title, description, tags, image: imageDataUrl});
+        ideas.push({ timestamp, title, description, tags, image: imageDataUrl});
         listIdeas();
         saveIdeas();
     };
@@ -43,18 +44,39 @@ function addIdea(event) {
 // Function to list ideas
 function listIdeas() {
     const output = document.getElementById('output');
-    output.innerHTML = '';
 
-    ideas.forEach((idea, index) => {
-        output.innerHTML += `<p><strong>${index + 1}. Title:</strong> ${idea.title}</p>`;
-        output.innerHTML += `<p><strong>Description:</strong> ${idea.description}</p>`;
-        output.innerHTML += `<p><strong>Tags:</strong> ${idea.tags.join(', ')}</p><br>`;
-        if (idea.image) {
-            output.innerHTML += `<img src="${idea.image}" width="200" height="auto" /><br>`;
-        }
-        output.innerHTML += `<button onclick="deleteIdea(${index})">Delete</button><br><br>`;
-    });
+    if (showIdeas) {
+        output.innerHTML = '';
+
+        ideas.forEach((idea, index) => {
+            output.innerHTML += `<p><strong>${idea.timestamp}<br><br>Title:</strong> ${idea.title}</p>`;        output.innerHTML += `<p><strong>Description:</strong> ${idea.description}</p>`;
+            output.innerHTML += `<p><strong>Tags:</strong> ${idea.tags.join(', ')}</p><br>`;
+            if (idea.image) {
+                output.innerHTML += `<img src="${idea.image}" width="200" height="auto" /><br>`;
+            }
+            output.innerHTML += `<button onclick="deleteIdea(${index})">Delete</button><br><br>`;
+        });
+    } else {
+        output.innerHTML = ''; // hide idea list
+    }
 }
+
+// Toggle visibility and button text for the idea list
+let showIdeas = false; // Variable to keep track of the idea list visibility
+
+document.getElementById('listButton').addEventListener('click', function () {
+    showIdeas = !showIdeas; // Toggle the state
+
+    const listButton = document.getElementById('listButton');
+    if (showIdeas) {
+        listButton.textContent = 'Hide Ideas'; // Change button text to "Hide Ideas"
+        listIdeas(); // Show the idea list
+    } else {
+        listButton.textContent = 'List Ideas'; // Change button text back to "List Ideas"
+        const output = document.getElementById('output');
+        output.innerHTML = ''; // Clear the idea list
+    }
+});
 
 // Function to save ideas to local storage
 function saveIdeas() {
@@ -77,5 +99,30 @@ function deleteIdea(index) {
     }
 }
 
+// Search Ideas function
+function searchIdeas () {
+    const searchTags = document.getElementById("searchTags").value.trim().toLowerCase();
+    const filteredIdeas = ideas.filter(idea => {
+        return idea.tags.some(tag => tag.toLowerCase().includes(searchTags));
+    });
+
+    listFilteredIdeas(filteredIdeas);
+}
+
+function listFilteredIdeas(filteredIdeas) {
+    const output = document.getElementById('output');
+    output.innerHTML = '';
+
+    filteredIdeas.forEach((idea, index) => {
+        output.innerHTML += `<p><strong>${idea.timestamp}<br><br>Title:</strong> ${idea.title}</p>`;        output.innerHTML += `<p><strong>Description:</strong> ${idea.description}</p>`;
+        output.innerHTML += `<p><strong>Tags:</strong> ${idea.tags.join(', ')}</p>`;
+        if (idea.image) {
+            output.innerHTML += `<img src="${idea.image}" width="200" height="auto" /><br>`;
+        }
+        output.innerHTML += `<button onclick="deleteIdea(${index})">Delete</button><br><br>`;
+    });
+}
+
 document.getElementById('ideaForm').addEventListener('submit', addIdea);
 document.getElementById('listButton').addEventListener('click', listIdeas);
+document.getElementById('searchButton').addEventListener('click', searchIdeas);
